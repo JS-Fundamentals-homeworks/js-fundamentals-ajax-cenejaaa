72f2b78eb12a8e7088cfc3fd8107c45fda1b6da0
petro <p.bondaruk@smartunex.com>
2025-10-20 11:07:38 +0200

    Task 1 + task 2

--- task1.js
+++ task1.js
@@ -5,3 +5,17 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+export const users = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    return await response.json();
+};
+
+users()
+    .then(data => {
+        const list = document.querySelector('.usersList');
+        list.innerHTML = data
+            .map(user => `<li>${user.name}</li>`)
+            .join('');
+    })
+    .catch(err => console.error('Fetch error:', err));
--- task2.js
+++ task2.js
@@ -5,3 +5,34 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+import { users } from './task1.js';
+
+const getUserByName = async () => {
+    const input = document.getElementById("userNameInput");
+    const userName = input.value.trim();
+
+    if (!userName) {
+        alert("Введіть ім'я користувача!");
+        return;
+    }
+
+    try {
+        const allUsers = await users();
+        const user = allUsers.find(u => u.name === userName);
+        const userCity = user.address.city;
+
+        if (user) {
+            document.getElementById("userCity").innerText = userCity;
+        }
+    } catch (err) {
+        console.error('Error fetching users:', err);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("getUserButton");
+    if (button) {
+        button.addEventListener("click", getUserByName);
+    }
+});
